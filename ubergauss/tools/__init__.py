860e11cb0472bf93ce8236ea6a957120af0f45aa
S Mautner <mautner@cs.uni-freiburg.de>
2021-10-25 09:26:36 +0000

    more nptools

--- ubergauss/tools/__init__.py
+++ ubergauss/tools/__init__.py
@@ -9,3 +9,17 @@ def xmap(func, iterable, processes=None):
   with Pool(processes, initializer=worker_init, initargs=(func,)) as p:
     return p.map(worker, iterable)
 
+import numpy as np
+from scipy.stats import spearmanr
+from scipy import sparse
+
+def spearman(x,y):
+    spear = lambda ft: np.abs(spearmanr(ft.T,y)[0])
+    x = zehidense(x)
+    re = list(map(spear,x.T))
+    return np.array(re)
+
+def zehidense(X):
+    if sparse.issparse(X):
+        return X.todense()
+    return X
