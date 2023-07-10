626e6d7149200ee1c646940123403518e156cdf7
S Mautner <mautner@cs.uni-freiburg.de>
2023-07-10 19:22:01 +0000

    adding group splitter

--- ubergauss/optimization/__init__.py
+++ ubergauss/optimization/__init__.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from itertools import product
 from ubergauss import tools as ut
+import numpy as np
+from sklearn.model_selection import  BaseCrossValidator
+
 
 
 def maketasks(param_dict):
@@ -30,3 +33,35 @@ def print(grid_df, score='score', showall=True):
             print (grid_df)
     else:
         print(grid_df)
+
+
+
+def contains(iter, element):
+    for e in iter:
+        if e == element:
+            return True
+    return False
+
+class groupedCV(BaseCrossValidator):
+    def __init__(self, n_splits):
+        self.n_splits = n_splits
+    def get_n_splits(self, X= None, y= None, groups = None):
+        return self.n_splits
+
+    # def arin(self,groupindex, testgrps):
+    #     return np.array([ contains(testgrps, a) for a in groupindex])
+
+    def arin_index(self,groupindex, testgrps):
+        return np.array([i for i,a in enumerate(groupindex) if contains(testgrps,a)])
+
+    def _iter_test_indices(self, X,y,groups):
+        groups = np.array(groups)
+        z = np.unique(groups)
+        np.random.shuffle(z)
+        if self.n_splits > 1:
+            for testgroups in np.array_split(z, self.n_splits):
+                res =  self.arin_index(groups, testgroups)
+                yield res
+        else:
+            test = np.array_split(z, 3)[0]
+            yield self.arin_index(groups, test)
