c1dcf75d941f2ab713c3914e09f9ab3dc5145b5b
S Mautner <mautner@cs.uni-freiburg.de>
2024-03-14 00:06:09 +0000

    pacmap loss except mids

--- ubergauss/csrjax.py
+++ ubergauss/csrjax.py
@@ -7,6 +7,10 @@ from jax.example_libraries.optimizers import adam
 from scipy.sparse import csr_matrix, coo_matrix
 from sklearn.decomposition import TruncatedSVD, PCA
 
+from jax import config
+config.update("jax_debug_nans", False)
+config.update("jax_enable_x64", True)
+
 def toCooTrip(X):
     coo=coo_matrix(X)
     i,j =coo.row, coo.col
@@ -20,10 +24,28 @@ def toCooTrip(X):
     return Map(jnp.array, [coo.row, coo.col, coo.data])
 
 
-def loss(embedding, triplets):
+def loss_justdistance(embedding, triplets):
     distance_pred = jnp.linalg.norm(embedding[triplets[0]] - embedding[triplets[1]], axis=1)
     losses = (distance_pred - triplets[2]) ** 2
     return jnp.mean(losses)
+
+def loss(embedding, triplets, badtrip):
+    # distance_pred = jnp.linalg.norm(embedding[triplets[0]] - embedding[triplets[1]], axis=1)
+    distance_pred = (embedding[triplets[0]] - embedding[triplets[1]]) ** 2
+    distance_pred = distance_pred.sum(axis=1)
+    loss_near = distance_pred/(distance_pred+10)
+
+    #jax.debug.print("{}", embedding[badtrip[0]] - embedding[badtrip[1]])
+    dp2 = (embedding[badtrip[0]] - embedding[badtrip[1]]) ** 2
+    dp2 = dp2.sum(axis=1)
+    # dp2 = jnp.linalg.norm(embedding[badtrip[0]] - embedding[badtrip[1]], axis=1)
+    loss_far = 1/(dp2+1)
+
+
+    ln = jnp.mean(loss_near)
+    lf = jnp.mean(loss_far)
+    # jax.debug.print("{}", ln+lf)
+    return ln+lf
 
 def inc_some(X,value=5,num=1):
     zero_indices = np.argwhere(X.toarray() == 0)
@@ -34,10 +56,12 @@ def inc_some(X,value=5,num=1):
     return X
 
 def embed(X,n_components = 2):
+    X,Xbad=X
     # inc_some(X, value= np.max(X) * 10,num = 10)
     coo_triplets = toCooTrip(X)
+    bad_triplets = toCooTrip(Xbad)
     # tsvd for init, should be nice as it plays well with sparse data
-    embedding = TruncatedSVD(n_components=n_components).fit_transform(X)
+    embedding = jnp.array(TruncatedSVD(n_components=n_components).fit_transform(X))
     # embedding = PCA(n_components=n_components).fit_transform(X.toarray())
 
     # Optimizer setup
@@ -48,7 +72,7 @@ def embed(X,n_components = 2):
 
     # Training loop
     for i in range(200):
-        gradients = grad_loss(get_params(opt_state), coo_triplets)
+        gradients = grad_loss(get_params(opt_state), coo_triplets,bad_triplets)
         opt_state = opt_update(i, gradients, opt_state)
 
     return get_params(opt_state)
